51d3dfca4a5f6133c05c93e0477bc3e5c413b6fb
Tanya Zakharova <sdfrthb23@yandex.ru>
2026-04-30 21:16:53 +0300

    fix: early exit and checking missed index

--- task-manager2.js
+++ task-manager2.js
@@ -1,64 +1,87 @@
 const tasks = [];
 const completedTaskCount = 0;
 
+const setTask1 = function (title, description = null) {
+  if (!title) {
+    console.log("Нельзя добавить пустую задачу");
+    return;
+  }
+  const task = {
+    title: title,
+    description: description,
+    isCompleted: false,
+    createdDate: new Date(),
+    completedDate: null,
+  };
+  tasks.push(task);
+};
+
 const setTask = function (title, description = null) {
   if (!title) {
     console.log("Нельзя добавить пустую задачу");
-  } else {
-    const task = {
-      title: title,
-      description: description,
-      isCompleted: false,
-      createdDate: new Date(),
-      completedDate: null,
-    };
-    tasks.push(task);
+    return;
   }
+  const task = {
+    title: title,
+    description: description,
+    isCompleted: false,
+    createdDate: new Date(),
+    completedDate: null,
+  };
+  tasks.push(task);
 };
 
 const showTask = function () {
   if (!tasks.length) {
-    console.log("Список задач пуст")
+    console.log("Список задач пуст");
+    return;
   }
-  else {
-  tasks.forEach((task, index) => {
-    console.log(`
+  tasks.forEach(
+    (
+      { title, description, isCompleted, createdDate, completedDate },
+      index
+    ) => {
+      console.log(`
     Задача №${index + 1}:
-    Название: ${task.title}
-    Описание: ${task.description}
-    Готовность: ${task.isCompleted ? "Готово" : "В процессе"}
-    Дата создания: ${task.createdDate}
-    Дата завершения: ${task.completedDate}
+    Название: ${title}
+    Описание: ${description}
+    Готовность: ${isCompleted ? "Готово" : "В процессе"}
+    Дата создания: ${createdDate}
+    Дата завершения: ${completedDate}
     `);
-  }); }
+    }
+  );
 };
 
 const completeTask = function (index) {
-  if (index >= 0 && index < tasks.length) {
-    const currTask = tasks[index];
-    currTask.isCompleted = true;
-    currTask.completedDate = new Date();
-    completedTaskCount += 1;
+  const currTask = tasks[index];
+
+  if (!currTask) {
+    console.log("Введён некорректный индекс");
     return;
   }
-  console.log("Введите корректный индекс");
+
+  currTask.isCompleted = true;
+  currTask.completedDate = new Date();
+  completedTaskCount += 1;
 };
 
 const deleteTask = function (index) {
-  if (index >= 0 && index < tasks.length) {
-    const currTask = tasks[index];
-    if (currTask.isCompleted) {
-      const answer = confirm("Таска еще не выполнена, удалить?");
-      if (answer) {
-        tasks.splice(index, 1);
-      }
-    }
+  const currTask = tasks[index];
+  if (!currTask) {
+    console.log("Введён некорректный индекс");
     return;
   }
-  console.log("Введите корректный индекс");
+  if (!currTask.isCompleted) {
+    const isConfirmed = confirm("Таска еще не выполнена, удалить?");
+    if (!isConfirmed) {
+      console.log("Удаление отменено");
+      return;
+    }
+  }
+  tasks.splice(index, 1);
 };
 
-const clearTasks = function() {
+const clearTasks = function () {
   arr.length = 0;
-}
-
+};
